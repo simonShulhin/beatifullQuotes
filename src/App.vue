5badd2d69b1a6f0b11328f8e16269034dfe4ebfa
Semen Shulhin <s.shulhin@viseven.com>
2020-03-18 18:11:57 +0200

    derictives

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,76 @@
 <template>
-  <div id="app">
-    <quotes-counter
-      :quoteCount="quotes.length"
-      :maxQuotes="maxValue"
-    ></quotes-counter>
-    <new-quote @quoteAdded="addNewQuote"></new-quote>
-    <grid-quotes>
-      <div class="quote" v-for="(quote, index) in quotes" :key="index" @click="deleteQuote(index)">
-        {{ quote }}
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-5 col-md-offset-3">
+        <h1>Built-in Directives</h1>
+        <p v-text="'Some text'"></p>
+        <p v-html="'<strong>Some text</strong>'"></p>
       </div>
-    </grid-quotes>
+    </div>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-5 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p v-highlight:backgroundColor.delayed="'red'">Color this</p>
+        <p
+          v-local-highlight:backgroundColor.delayed.blink="{
+            mainColor: 'green',
+            secondColor: 'red',
+            delay: 200,
+          }"
+        >
+          Color this
+        </p>
+      </div>
+    </div>
+    <hr />
   </div>
 </template>
 
 <script>
-import GridQuotes from './components/GridQuotes.vue';
-import QuotesCounter from './components/QuotesCounter.vue';
-import NewQuote from './components/NewQuote.vue';
-
 export default {
   name: 'App',
-  components: {
-    GridQuotes,
-    QuotesCounter,
-    NewQuote,
-  },
   data: () => ({
-    maxValue: 10,
-    quotes: ['Start beautifull quote!', 'Test'],
-  }),
-
-  methods: {
-    addNewQuote(quote) {
-      this.quotes.length === this.maxValue
-        ? alert(`It's no place for new quotes. Please delete old one!`)
-        : this.quotes.push(quote);
+    highlighting: {
+      mainColor: 'green',
+      secondColor: 'red',
+      delay: '1000',
     },
-
-    deleteQuote(i) {
-      this.quotes.splice(i, 1);
-    }
+  }),
+  directives: {
+    'local-highlight': {
+      bind(el, binding) {
+        let delay = 0;
+        if (binding.modifiers['delayed']) {
+          delay = 3000;
+        }
+        if (binding.modifiers['blink']) {
+          let mainColor = binding.value.mainColor;
+          let secondColor = binding.value.secondColor;
+          let currentColor = mainColor;
+          setTimeout(() => {
+            setInterval(() => {
+              currentColor == secondColor
+                ? (currentColor = mainColor)
+                : (currentColor = secondColor);
+              binding.arg === 'backgroundColor'
+                ? (el.style.backgroundColor = currentColor)
+                : (el.style.color = currentColor);
+            }, binding.value.delay);
+          }, delay);
+        } else {
+          setTimeout(() => {
+            if (binding.arg === 'backgroundColor') {
+              el.style.backgroundColor = binding.value.mainColor;
+            } else {
+              el.style.color = binding.value.mainColor;
+            }
+          }, delay);
+        }
+      },
+    },
   },
 };
 </script>
 
-<style>
-#app {
-  margin: 40px;
-}
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.directive('highlight', {
+  bind(el, binding) {
+    let delay = 0;
+    if (binding.modifiers['delayed']) {
+      delay = 3000;
+    }
+    setTimeout(() => {
+      if (binding.arg === 'backgroundColor') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
+  },
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
